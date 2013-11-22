c3312d51c96fcfc962a71722c674628668c09781
Erik Peldan <epe@trifork.com>
2013-11-22 18:13:18 +0100

    function composition

--- fun_utils.lisp
+++ fun_utils.lisp
@@ -8,3 +8,23 @@
     (f &rest partial-args)
   (lambda (&rest rest)
     (apply f (append partial-args rest))))
+
+
+(defun compose
+    (&rest funcs)
+  (let ((f (car funcs))
+	(gs (cdr funcs)))
+    (if (null gs)
+	f
+	(lambda (&rest args)
+	  (let ((g (apply #'compose gs)))
+	    (funcall f (apply g args)))))))
+
+(let ((f (partial #'+ 3)))
+  (funcall f 4))
+
+(let ((f (partial #'* 3 4)))
+  (funcall f 5))
+
+(let ((f (compose (partial #'+ 3) (partial #'* 4))))
+  (funcall f 5))
