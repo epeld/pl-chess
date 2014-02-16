6e234775bab0d5409954051f7e2ed7928a4dddd0
Erik Peldan <epe@trifork.com>
2014-02-16 22:02:28 +0100

    add new chess package, add functionality to seq

--- packages.lisp
+++ packages.lisp
@@ -6,7 +6,7 @@
   (:use :common-lisp
 	:peldan.util.common
 	:it.bese.FiveAM)
-  (:export take drop prefixp replicate split))
+  (:export take drop prefixp replicate split insert))
 
 (defpackage :peldan.util.parse
   (:use :common-lisp)
@@ -35,3 +35,8 @@
 	:peldan.util.seq
 	:peldan.pgn
 	:it.bese.FiveAM))
+
+(defpackage :peldan.chess
+  (:use :common-lisp
+	:peldan.util.common)
+  )
