5033b02e9cf93b506586f3d3f311a1e59fbdbe16
Erik Peldan <epe@trifork.com>
2015-08-23 15:28:19 +0200

    prolog branch

--- list.lisp
+++
@@ -1,1 +0,0 @@
-(in-package :peldan.util.list)
--- parse.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :peldan.util.parse)
-
-(defun number-char-p
-    (c)
-  (let ((num (- (char-int c) (char-int #\0))))
-    (when (and (<= 0 num) (<= num 9))
-      num)))
