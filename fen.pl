ad330be2ec386dc6147a83c9a7a7a264f93c18a4
Erik Peldan <epe@trifork.com>
2015-08-25 22:32:33 +0200

    board can be parsed into list of pieces

--- fen.pl
+++ fen.pl
@@ -1,32 +1,24 @@
 
 use_module(listutils).
+use_module(square).
 
 % rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
 
 example_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2").
-parsed_board(example_board, FEN) :-
-    example_fen(FEN).
 
-example_board(B) :- parsed_board(B, F), example_fen(F).
+
+test(Position) :- string_chars(FEN, Chars), example_fen(FEN), fen_string(Chars, Position).
+
+test_board(Board) :- fen_board("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R", Board).
+% test_board(Board) :- string_chars("5r2", Chars), fen_single_row(Chars, Board).
 
 fen_string(FEN, Position) :-
-    fen_parts(FEN, Position),
+    string_chars(FEN, Chars), fen_parts(Chars, Parts),
     Parsers = [fen_board, fen_turn, fen_castling, fen_passant, fen_movenumber, fen_movenumber],
-    maplist(call, Parsers, Position).
+    maplist(call, Parsers, Parts, Position).
     
-fen_string2(FEN, Position) :-
-    fen_parts(FEN, [BoardPart, TurnPart, CastlingPart, PassantPart,
-		    FullMovePart, HalfMovePart]),
-    fen_board(BoardPart, Board),
-    fen_turn(TurnPart, Turn),
-    fen_castling(CastlingPart, Castling),
-    fen_passant(Passant, PassantPart),
-    fen_movenumber(FullMove, FullMovePart),
-    fen_movenumber(HalfMove, HalfMovePart),
-    Position = [Board, Turn, Castling, Passant, FullMove, HalfMove].
 
-
-fen_castling(S, []) :- string_chars("-", S).
+fen_castling(['-'], []).
 fen_castling(A0, B0) :- 
     length(A0, N), N > 0,
     string_chars("KQkq", KQkq), listutils:sublist(KQkq, A0),
@@ -37,26 +29,31 @@ fen_castling_right(Char, [Color, Piece]) :- member(Piece, [king, queen]), fen_pi
 
 
 fen_board(BoardPart, Board) :-
-    fen_board_rows(BoardPart, ReversedRows),
+    fen_board_rows(BoardPart, Rows),
     reverse(ReversedRows, Rows),
-    maplist(fen_single_row, Rows, ParsedRows),
-    board_rows(Board, ParsedRows).
+    maplist(fen_single_row, ReversedRows, ParsedRows),
+    append(ParsedRows, Board).
 
-fen_turn("w", white).
-fen_turn("b", black).
 
-fen_passant("-", nothing).
-fen_passant(Term, Square) :- pgn_square(Square, Term).
+fen_turn('w', white).
+fen_turn('b', black).
+
+
+fen_passant('-', nothing).
+fen_passant(SquareChars, Square) :- square(SquareChars, Square).
+
 
 fen_movenumber(Number, Term) :- parsed_number(Term, Number).
 
+
 fen_parts(FEN, Parts) :-
-    split_string(FEN, " ", "", Parts),
-    length(Parts, 6).
+    length(Parts, 6),
+    split_chars(FEN, ' ', Parts).
+
 
 fen_board_rows(BoardPart, Rows) :-
-    split_string(BoardPart, "/", "", Rows),
-    length(Rows, 8).
+    length(Rows, 8),
+    split_chars(BoardPart, '/', Rows).
 
 
 fen_single_row(A, B) :- fen_single_row(A, B, 8).
@@ -64,12 +61,12 @@ fen_single_row(A, B) :- fen_single_row(A, B, 8).
 fen_single_row(_, [], 0).
 
 fen_single_row([Char | Rest], [Piece | Row], N0) :-
-    fen_piece([Char], Piece),
+    fen_piece(Char, Piece),
     N is N0 - 1,
     fen_single_row(Rest, Row, N).
 
 fen_single_row([NumberChar | RestChars], Row, N0) :-
-    number_term([NumberChar], M), between(1, 8, M), filled(nothing, Empties, M),
+    char_type(NumberChar, digit(M)), between(1, N0, M), filled(nothing, Empties, M),
     N is N0 - M,
     append(Empties, Rest, Row),
     fen_single_row(RestChars, Rest, N).
