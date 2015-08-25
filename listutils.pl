bbc17c92e40ffdc5cc4ec2559a0a92ca81ae7912
Erik Peldan <epe@trifork.com>
2015-08-25 22:32:20 +0200

    write split predicate for lists of characters

--- listutils.pl
+++ listutils.pl
@@ -8,3 +8,17 @@ sublist(_, []).
 filled(_, []).
 filled(X, [X | L]) :- filled(X, L).
 filled(X, L, N) :- length(L, N), filled(X, L).
+
+
+split_chars(Full, Sep, Parts) :-
+    ground(Full),
+    atom_chars(FullAtom, Full),
+    atomic_list_concat(AtomParts, Sep, FullAtom),
+    maplist(atom_chars, AtomParts, Parts).
+
+
+split_chars(Full, Sep, Parts) :-
+    ground(Parts),
+    maplist(atom_chars, AtomParts, Parts),
+    atomic_list_concat(AtomParts, Sep, FullAtom),
+    atom_chars(FullAtom, Full).
