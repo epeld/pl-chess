34d05b7e0781944dbf39a916a299bbcc7d0ca6bf
Erik Peldan <epe@trifork.com>
2015-08-30 12:35:39 +0200

    Add file contsraints

--- square.pl
+++ square.pl
@@ -13,18 +13,28 @@ file(6, 'g').
 file(7, 'h').
 
 
+files(X) :- string_chars("abcdefgh", X).
+
+
 square_file_coord(Sq, Coord) :- square_file(Sq, File), file(Coord, File).
 square_rank_coord(Sq, Coord) :- square_rank(Sq, Rank), rank(Coord, Rank).
+
 
 square_file([File, _], File).
 square_rank([_, Rank], Rank).
 
 
-square_chars([File, Rank], [File, RankChar]) :- char_type(RankChar, digit(Rank)).
+square_chars([File, Rank], [File, RankChar]) :- 
+    files(Files), 
+    member(File, Files), 
+    char_type(RankChar, digit(Rank)).
+
 
 square_string(Sq, S) :- string(S), string_chars(S, Chars), square_chars(Sq, Chars).
 
+
 square([CoordX, CoordY], [File, Rank]) :- file(CoordX, File), rank(CoordY, Rank).
+
 
 :- begin_tests(square).
 
