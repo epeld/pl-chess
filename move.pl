ba840df20c9b30b1ad9ff3b4e593f19ee7d231cf
Erik Peldan <epe@trifork.com>
2015-09-01 22:15:40 +0200

    try to give structure to move-representation

--- move.pl
+++ move.pl
@@ -3,8 +3,32 @@
 :- use_module(library(clpfd)).
 :- use_module(square).
 
+
+% a piece move will be represented by a list of 3-4 elements
 move([pawn | Rest], [pawn | Rest]) :- length(Rest, 4).
 move([Officer | Rest], [Officer | Rest]) :- officer(Officer), length(Rest, 3).
+
+
+% Non-castling move:
+piece_move([Piece | _]) :- piece_move(Piece).
+
+
+castling_move([castling, Side]) :- castling_side(Side).
+
+
+moved_piece(Move, Piece) :- piece_move(Move), nth0(0, Move, Piece).
+
+source_indicator(Move, Source) :- piece_move(Move), nth0(1, Move, Source).
+
+move_type(Move, MoveType) :- piece_move(Move), nth0(1, Move, MoveType).
+
+destination(Move, Destination) :- piece_move(Move), nth0(1, Move, MoveType).
+
+promotion(Move, Promotion) :- piece_move(Move), nth0(1, Move, MoveType).
+
+
+piece(pawn).
+piece(Officer) :- officer(Officer).
 
 
 officer(bishop).
@@ -12,6 +36,10 @@ officer(knight).
 officer(rook).
 officer(king).
 officer(queen).
+
+
+castling_side(queenside).
+castling_side(kingside).
 
 
 knights_jump(Square1, Square2) :-
