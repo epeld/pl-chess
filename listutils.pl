2472d0db5ef0b6aeb69a22d5348a47289edd0150
Erik Peldan <epe@trifork.com>
2015-09-03 23:06:36 +0200

    Write a list replace function

--- listutils.pl
+++ listutils.pl
@@ -1,5 +1,7 @@
 :- module(listutils, [sublist/2, filled/2]).
 
+:- use_module(library(clpfd)).
+
 % sublist(L, L2) means "L2 is a sublist of L"
 sublist([A | As], [A | Bs]) :- sublist(As, Bs). 
 sublist([_ | As], [B | Bs]) :- sublist(As, [B | Bs]). 
@@ -9,3 +11,10 @@ sublist(_, []).
 filled(_, []).
 filled(X, [X | L]) :- filled(X, L).
 filled(X, L, N) :- length(L, N), filled(X, L).
+
+
+replace(_, [], [], _).
+replace(0, [_ | L], [El | L], El).
+replace(Ix, [A | L], [A | L2], El) :-
+    Ix #> 0, Ix2 #= Ix - 1,
+    replace(Ix2, L, L2, El).
