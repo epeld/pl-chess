b1d462d42684014c9f047f121b4db50ad9e4b674
Erik Peldan <epe@trifork.com>
2015-09-04 21:37:55 +0200

    work on move inference-predicates

--- moveinterence.pl
+++ moveinterence.pl
@@ -3,38 +3,45 @@
         uniquely_determined/2
     ]).
 
-:- use_module(move).
+:- use_module(pgnmove).
 :- use_module(movement).
 :- use_module(square).
 :- use_module(listutils).
 
 
-% A move is uniquely determined in a given position
-% whenever there is only one possible source square
-% that it can have
-uniquely_determined(Move, Position) :-
-    bagof(X, possible_source_square(Move, Position, X), [_]).
+% Determine the least sufficient source indicator that
+% uniquely determines FullMove in Position,
+% Example:
+% given FullMove = Ne2xd4, Indicator might be 'e',
+% meaning Nexd4 would be enough to determine the intended move
+sufficient_indicator(Indicator, Position, FullMove) :-
+    bagof(Indicator, blabla, _). % TODO
 
 
+% A Move is uniquely determined in a given position
+% whenever it has only one legal_source_square.
+uniquely_determined_source(Move, Position, UniqueSource) :-
+    bagof(X, legal_source_square(Move, Position, X), [UniqueSource]).
+
+
+% A legal source square is a candidate source square
+% that, when when used with Move, results in a legal position
 legal_source_square(Move, Pos, Square) :-
-    full_move(Move, FullMove, Square),
+    candidate_square(Move, Pos, Square),
+    pgnmove:source_square(Move, Pos, FullMove),
     legal(FullMove, Pos).
 
 
-% FullMove is an 'Upgrade' of Move with source_indicator set to Square
-full_move(Move, FullMove, Square) :-
-    piece_move(Move),
-    possible_source_square(Move, Pos, Square),
-    replacement_at(1, Move, FullMove, Square).
+% TODO create legality-module
+% TODO determine if a move is legal in a given position
+legal(_, _).
 
 
-% A square is a possible source square of a move
-% if it is occupied by the right piece (of the right color)
-% and the square matches the move's source indicator
-% TODO: and the resulting positon would be legal
-possible_source_square(Move, Pos, Square) :-
-    compatible_source_square(Move, Square),
-    moved_piece(Move, Piece),
+% A candidate source square of a Move is a source square
+% that is occupied by a piece that is capable of
+% performing the Move in question
+candidate_source_square(Move, Pos, Square) :-
+    pgnmove:source_square(Move, Square),
+    pgnmove:moved_piece(Move, Piece),
     position_turn(Pos, Color),
-    board_occupant(Pos, Square, [Piece, Color]),
-    legal(Move, Pos).
+    board_occupant(Pos, Square, [Piece, Color]).
