4a6efb311422d8dda46fe519372757fa1fc4126f
Erik Peldan <epe@trifork.com>
2015-12-14 22:24:49 +0100

    Attempt to implement 'between-square-generation' using setof/3

--- movement.pl
+++ movement.pl
@@ -24,7 +24,7 @@ distance(Square1, Square2, Range) :-
     square([X1, Y1], Square1), 
     square([X2, Y2], Square2),
 
-    abs(X2 - X1) + abs(Y2 - Y1) #=< Range.
+    abs(X2 - X1) + abs(Y2 - Y1) #= Range.
 
 
 diagonal(Square1, Square2) :-
@@ -50,9 +50,104 @@ diagonal(Square1, Square2, SquareOnDiagonal) :-
 
 horizontal(Square1, Square2) :-
     square([_, Y1], Square1), 
-    square([_, Y1], Square2).
+    square([_, Y1], Square2),
+
+    not_equal(Square1, Square2).
+
+horizontal(Square1, Square2, Between) :-
+    horizontal(Square1, Square2),
+    horizontal_between(Square1, Square2, Between).
+
+horizontal_between(Square1, Square2, Between) :- left_of(Square1, Between), right_of(Square2, Between).
+horizontal_between(Square1, Square2, Between) :- left_of(Square2, Between), right_of(Square1, Between).
     
 
 vertical(Square1, Square2) :-
     square([X2, _], Square1), 
-    square([X2, _], Square2).
+    square([X2, _], Square2),
+
+    not_equal(Square1, Square2).
+
+vertical(Square1, Square2, Between) :-
+    vertical(Square1, Square2),
+    vertical_between(Square1, Square2, Between).
+
+vertical_between(Square1, Square2, Between) :- below(Square1, Between), above(Square2, Between).
+vertical_between(Square1, Square2, Between) :- below(Square2, Between), above(Square1, Between).
+
+
+straight(Square1, Square2, Between) :- vertical(Square1, Square2, Between) ; horizontal(Square1, Square2, Between).
+
+can_reach(queen, Source, Target, Between) :- can_reach(rook, Source, Target, Between) ; can_reach(bishop, Source, Target, Between).
+
+can_reach(king, Source, Target, []) :- distance(Source, Target, 2), diagonal(Source, Target).
+can_reach(king, Source, Target, []) :- distance(Source, Target, 1), straight(Source, Target).
+
+can_reach(knight, Source, Target, []) :- knights_jump(Source, Target).
+
+can_reach(bishop, Source, Target, Between) :- setof(X, diagonal(Source, Target, X), Between).
+can_reach(bishop, Source, Target, []) :- distance(Source, Target, 2), diagonal(Source, Target). 
+
+can_reach(rook, Source, Target, Between) :- setof(X, straight(Source, Target, X), Between).
+can_reach(rook, Source, Target, []) :- distance(Source, Target, 1), straight(Source, Target).
+
+
+can_capture(Officer, Source, Target, Between) :- officer(Officer), can_reach(Officer, Source, Target, Between).
+
+
+% Determine if a piece can capture a given square
+% First argument is a piece
+% Second argument is the source square
+% Third argument is target square
+% Fourth argument will be a list of all squares in between source and target
+piece_can_capture([_, Officer], Source, Target, Between) :- 
+    officer(Officer), 
+    can_capture(Officer, Source, Target, Between).
+
+piece_can_capture([white, pawn], Source, Target, []) :-
+    distance(Source, Target, 2),
+    diagonal(Source, Target),
+    below(Source, Target, Between).
+
+piece_can_capture([black, pawn], Source, Target, []) :-
+    distance(Source, Target, 2),
+    diagonal(Source, Target),
+    below(Target, Source).
+
+
+% Determine if a piece can reach a given square
+% First argument is a piece
+% Second argument is the source square
+% Third argument is target square
+% Fourth argument will be a list of all squares in between source and target
+piece_can_reach([_, Officer], Source, Target, Between) :-
+    officer(Officer),
+    can_reach(Officer, Source, Target, Between).
+
+piece_can_reach([white, pawn], Source, Target, []) :-
+    distance(Source, Target, 1)
+    vertical(Source, Target),
+    below(Source, Target).
+
+piece_can_reach([black, pawn], Source, Target, []) :-
+    distance(Source, Target, 1)
+    vertical(Source, Target),
+    below(Target, Source).
+
+
+not_equal([X1, Y1], [X2, Y2]) :- 
+    X1 #\= X2 ; Y1 #\ Y2.
+
+
+below([_, Y1], [_, Y2]) :- Y1 #< Y2.
+above([_, Y1], [_, Y2]) :- Y1 #> Y2.
+
+left_of([X1, _], [X2, _]) :- X1 #< X2.
+right_of([X1, _], [X2, _]) :- X1 #> X2.
+
+
+officer(rook).
+officer(bishop).
+officer(queen).
+officer(king).
+officer(knight).
