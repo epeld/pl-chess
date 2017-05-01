bc4e00360e77b62ea56524cdfa991b0961d1025e
Erik Peldan <erik.peldan@gmail.com>
2017-05-01 13:53:08 +0200

    Write list_replace function

--- position.pl
+++ position.pl
@@ -1,3 +1,15 @@
 
 :- module(position, []).
 
+%
+%  This module handles updating a position, i.e after a move has been made
+%
+
+list_replace(Ix, Item, [X | List], [ X | Result]) :-
+  nonvar(Ix), nonvar(Item),
+  Ix > 0,
+  succ(Ix0, Ix),
+  list_replace(Ix0, Item, List, Result).
+
+list_replace(0, Item, [_ | List], [Item | List]).
+
