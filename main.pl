ad803853df9d702a234b853d36f0f450f5fae8ea
Erik Peldan <erik.peldan@gmail.com>
2017-05-05 21:45:32 +0200

    Write a command line program that makes use of the chess predicates

---
+++ main.pl
@@ -0,0 +1,55 @@
+
+:- module(main, []).
+
+evaluate(position, P, _, P).
+
+evaluate(move, [Move | Moves], P, P2) :-
+  pgn:full_move(P, Move, FullMove),
+  pgn:pgn_string(FullMove, Str),
+  format("~s\n", [Str]), % TODO plug in different notations here
+  position:position_after(FullMove, P, P1),
+  evaluate(move, Moves, P1, P2).
+
+evaluate(move, [], P, P).
+
+
+command(position, P) -->
+  position(P).
+
+command(move, Moves) -->
+  move(Moves).
+
+
+position(P) -->
+  "position ", fen:position(P).
+
+move(Moves) -->
+  "move ", many_moves(Moves).
+
+
+many_moves([Move | Moves]) -->
+  pgn:move(Move),
+
+  ( " ", many_moves(Moves)
+  ; { Moves = [] } ).
+
+
+repl :-
+  fen:initial_position(P),
+  repl(P).
+
+repl(Position) :-
+  fen:string(Position, Str),
+  format("Position: ~s\n", [Str]),
+  format("> "),
+  read_line_to_codes(user_input, Line),
+
+  command(Command, Arg, Line, []),
+
+  evaluate(Command, Arg, Position, Position2), !,
+  repl(Position2).
+
+repl(Position) :-
+  !,
+  format("Error! Something went wrong. \n"),
+  repl(Position).
