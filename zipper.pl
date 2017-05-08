53d9d8745d0d9774ab9c7ff4e9e40e08d108f5a8
Erik Peldan <erik.peldan@gmail.com>
2017-05-08 19:34:08 +0200

    Write an implementation of a zipper data structure

---
+++ zipper.pl
@@ -0,0 +1,28 @@
+:- module(zipper, []).
+
+%
+% This module defines a data structure for moving back and forth along a line of
+% - say - positions in a chess game!
+%
+
+zsingleton([Value, [], []], Value).
+
+zappend(Items,
+        [Value, Backward, Forward],
+        [Value, Backward, Forward2]) :-
+  
+  append(Forward, Items, Forward2).
+
+
+zbackward([Value, [Item | Backward], Forward], [Item, Backward, [Value | Forward]]).
+
+
+zforward([Value, Backward, [Item | Forward]], [Item, [ Value | Backward ], Forward]).
+
+
+zipper([Value, Backward, Forward], Value, Backward, Forward).
+
+zvalue([Value, _, _], Value).
+
+% zput/3 is like zappend/3 except it expects the zipper to be forwarded to the end
+zput([V, History, []], [V, History, Values], Values).
