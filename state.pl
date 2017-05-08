98f3798335e4b2e8ffc74c49cc2c42e82f65f67c
Erik Peldan <erik.peldan@gmail.com>
2017-05-08 22:14:03 +0200

    Work on representing the editor state explicitly in a structure

---
+++ state.pl
@@ -0,0 +1,62 @@
+
+:- module(state, []).
+
+%
+% This Module represents the editor state when viewing a PGN file.
+% You can move forwards and backwards in the game history,
+% as well as inspect Position, Moves and Comments along the way
+%
+
+initial_state([state, Zipper, []]) :-
+  fen:initial_position(P),
+  zipper:zsingleton(Zipper, [P, initial, "Starting Position"]).
+
+
+custom_state([state, Zipper, []], P) :-
+  \+ fen:initial_state(P),
+  zipper:zsingleton(Zipper, [P, initial, "Custom Starting Position"]).
+
+custom_state(State, P) :-
+  initial_state(State),
+  state_position(State, P).
+  
+
+
+state_parts([state, Zipper, []], Position, Move, Comment) :-
+  zipper:zvalue(Zipper, [Position, Move, Comment]).
+
+
+state_position(State, Position) :-
+  state_parts(State, Position, _, _).
+
+state_comment(State, Comment) :-
+  state_parts(State, _, _, Comment).
+
+state_last_move(State, Move) :-
+  state_parts(State, _, Move, _).
+
+
+state_move(Moves, State, StateN) :-
+  % Extract the current position
+  State = [state, Zipper, Options],
+  state_position(State, Position),
+
+  % Apply the move to it..
+  pgn:legal_position_after(Move, Position, Position2),
+
+  % Construct a new 'node' to put back into the zipper..
+  zipper:zput(Zipper, Zipper1, [[Position2, Move, ""]]),
+
+  % and forward so that the new position is active
+  zipper:forward(Zipper1, Zipper2),
+
+  State2 = [state, Zipper2, Options],
+  state_move(Moves, State2, StateN).
+
+
+state_forward([state, Zipper, Options], [state, Zipper2, Options]) :-
+  zipper:forward(Zipper, Zipper2).
+
+
+state_truncate([state, Zipper, Options], [state, Zipper2, Options]) :-
+  zipper:zput(Zipper2, Zipper, _).
