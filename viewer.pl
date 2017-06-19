6c1323dabe40018818f602fa6085ef5ded888e69
Erik Peldan <erik.peldan@gmail.com>
2017-06-19 22:21:41 +0200

    Start outlining viewer state

--- viewer.pl
+++ viewer.pl
@@ -7,7 +7,12 @@
 :- use_module(uci).
 
 
-initial_state(state(Zipper, Options)).
+initial_state(state(Zipper, Options)) :-
+  zsingleton(Zipper, Pos),
+  initial_position(Pos),
+  initial_options(Options).
+
+initial_position(todo).
 
 
 repl(State) :-
@@ -16,38 +21,31 @@ repl(State) :-
   repl(State2).
 
 
+process_message(Msg, state(Z, Options), state(Z2, Options)) :-
+  zipper_message(Msg, Z, Z2).
+
+process_message(Msg, state(Z, Options), state(Z, Options2)) :-
+  option_message(Msg, Options, Options2).
+
 %
 % Game State Modifiers/Accessors
 %
-process_message(forward,
-                state(Z, Options),
-                state(Z2, Options)) :-
-  
+zipper_message(forward, Z, Z2) :-
   zipper2:zforward(Z, Z2).
 
-
-process_message(backward,
-                state(Z, Options),
-                state(Z2, Options)) :-
-
+zipper_message(backward, Z, Z2) :-
   zipper2:zforward(Z2, Z).
 
-
-process_message(move_number(N),
-                Z, Z2) :-
-
+zipper_message(move_number(N), Z, Z2) :-
   zipper2:zrewind(Z, Z0),
-  process_message(forward(N), Z0, Z2).
+  zipper_message(forward(N), Z0, Z2).
 
+zipper_message(last, Z, Z2) :- zipper2:zfastforward(Z, Z2).
 
-process_message(last, Z, Z2) :-
-  zipper2:zfastforward(Z, Z2).
-
-
-process_message(first, Z, Z2) :-
-  zipper2:zrewind(Z, Z2).
+zipper_message(first, Z, Z2) :- zipper2:zrewind(Z, Z2).
 
 
 %
 %  Options
 %
+option_message(none, O, O). % TODO
