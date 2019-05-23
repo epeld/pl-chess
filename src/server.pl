7924e6c0ea2de2842601fd70c1f5f8039a688e3a
Erik Peldan <erik.peldan@gmail.com>
2019-05-23 22:15:39 +0200

    Need HTTP endpoint that handles POST params

--- src/server.pl
+++ src/server.pl
@@ -2,11 +2,13 @@
 
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_parameters)).
 
-:- use_module(services/fen_service, [initial_fen_string/1]).
+:- use_module(services/fen_service, [initial_fen_string/1, parse_string/2]).
 
 
 :- http_handler(/, say_hi, []).
+:- http_handler('/pgn/move', make_move, []).
 
 server(Port) :-
   http_server(http_dispatch, [port(Port)]).
@@ -16,4 +18,15 @@ say_hi(_Request) :-
 
   initial_fen_string(S),
   format('~s', [S]).
+
+
+make_move(Request) :-
+  http_parameters(Request,
+                  [
+                    fen(Fen, []),
+                    move(Move, [])
+                  ]),
+  format('Content-type: text/plain~n~n'),
+
+  format('~s ~s', [Fen, Move]).
 
