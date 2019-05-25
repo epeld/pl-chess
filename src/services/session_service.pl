a12532ac51ab9d1f324b335bcbb4dc1d36da2c65
Erik Peldan <erik.peldan@gmail.com>
2019-05-25 10:48:44 +0200

    Need module for keeping track of chess game 'sessions'

---
+++ src/services/session_service.pl
@@ -0,0 +1,43 @@
+:- module(session_service,
+          [
+            new_session/1,
+            delete_session/1
+            session/1,
+            session/2
+          ]).
+:- use_module(library(random)).
+
+:- set_prolog_flag(double_quotes, codes).
+
+:- dynamic session/2.
+
+session(SessionId) :-
+  session(SessionId, _).
+
+new_session(SessionId) :-
+  uuid(SessionId),
+  with_mutex(
+    session,
+    store_session(SessionId)
+  ).
+
+delete_session(SessionId) :-
+  with_mutex(
+    session,
+    retract(session(SessionId, _))
+  ).
+
+store_session(SessionId) :-
+  \+ session(SessionId),
+  InitialFEN = "TODO",
+  assertz(session(SessionId, InitialFEN)).
+
+
+
+uuid(Id) :-
+  length(IdC, 10),
+  maplist(random_element, IdC),
+  atom_codes(Id, IdC).
+
+random_element(Element) :-
+  random_member(Element, "0123456789abcdefghijklmnopqrstuvwxyz").
